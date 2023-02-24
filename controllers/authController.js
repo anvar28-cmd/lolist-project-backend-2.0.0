eb643a3a04e720d91592c08113f367c7db7d6b81
Anvar Gulomov <anvar.gulomov.97@gmail.com>
2023-02-24 06:47:46 -0500

    username validation add

--- controllers/authController.js
+++ controllers/authController.js
@@ -23,13 +23,56 @@ exports.login = async (req, res) => {
 exports.signup = async (req, res) => {
   const { username, password, name } = req.body;
 
-  // TODO: Validate if username exists
+  const isValid = await knex('users').where({ username }).first() ? false : true;
 
-  await knex("users").insert({ username, password, name });
+  if (isValid) {
+    try {
+      await knex('users').insert({ username, password, name });
+      const user = await knex("users").where({ username }).first();
+      const token = jwt.sign({ username, id: user.id }, secret);
+      res.json({ ...user, token });
+    } catch(error) {
+      res.status(401).json({error: { message: "Something went wrong" }});
+    }
+  } else {
+    res.status(401).json({error: { message: "Username already exists" }});
+  }
 
-  const user = await knex("users").where({ username }).first();
+  // TAMOM?? Proverka kunam chi hato ku tam!
+  //ne rabotaet
 
-  const token = jwt.sign({ username, id: user.id }, secret);
-  console.log(token);
-  res.json({ ...user, token });
+
+  // const isValid = false;
+
+  // if (isValid) {
+  //   storeUser();
+  // } else {
+  //   return errorMessage;
+  // }
+
+  // fahmidi???????net
+
+
+  // try {
+
+  //   const user = await knex('users').where({ username }).first();
+
+  //   const token = jwt.sign({ username, id: user.id }, secret);
+    
+  //   res.json({ ...user, token });
+  // } catch (err) {
+  //   if (err.code === 'ER_DUP_ENTRY' || err.errno === 1062) {
+  //        res.status(401).json({
+  //     error: {
+  //       message: "Username already exists"
+  //     }
+  //   })
+  //   } else {
+  //     res.status(401).json({
+  //       error: {
+  //         message: "Something went wrong"
+  //       }
+  //     })
+  //   }
+  // }
 };
